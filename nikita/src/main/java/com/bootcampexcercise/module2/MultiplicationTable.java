32f743216d6f2c58dfbb41a3c6bc5afb49bfc43d
naujokasnikita-ctrl <naujokasnikita@gmail.com>
2026-04-08 09:23:35 +0300

    day 2

---
+++ nikita/src/main/java/com/bootcampexcercise/module2/MultiplicationTable.java
@@ -0,0 +1,8 @@
+package com.bootcampexcercise.modul.exercises;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+    int firstnum = 11;
+    int secondnum = 20
+    }
+}
